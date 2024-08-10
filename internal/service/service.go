44217622a30fa65ae5fd23c9f678012bc19321cc
Aleksey Popov <asepopov@avito.ru>
2024-08-10 21:23:01 +0300

    add logging

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,13 +2,12 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	auth_proto "github.com/s21platform/auth-proto/auth-proto"
 	"github.com/s21platform/auth-service/internal/config"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+	"log"
 	"strings"
 	"time"
 )
@@ -22,23 +21,26 @@ type Server struct {
 }
 
 func (s *Server) Login(ctx context.Context, req *auth_proto.LoginRequest) (*auth_proto.LoginResponse, error) {
-	md, _ := metadata.FromIncomingContext(ctx)
-	fmt.Println(md.Get("trace-id"))
+	//md, _ := metadata.FromIncomingContext(ctx)
+	//fmt.Println(md.Get("trace-id"))
 	username := req.Username
 	if !strings.HasSuffix(req.Username, "@student.21-school.ru") {
 		username += "@student.21-school.ru"
 	}
 	is, err := s.communityS.CheckPeer(ctx, username)
 	if err != nil {
+		log.Println("Error checking user", err)
 		return nil, err
 	}
 
 	if !is {
+		log.Println("User isn't a community")
 		return nil, status.Errorf(codes.FailedPrecondition, "Вы не являетесь участником s21")
 	}
 
 	t, err := s.schoolS.DoLogin(ctx, username, req.Password)
 	if err != nil {
+		log.Println("Error do login", err)
 		return nil, err
 	}
 
@@ -51,6 +53,7 @@ func (s *Server) Login(ctx context.Context, req *auth_proto.LoginRequest) (*auth
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, data)
 	tokenString, err := token.SignedString([]byte(s.cfg.Service.Secret))
 	if err != nil {
+		log.Println("Error signing token", err)
 		return nil, err
 	}
 	return &auth_proto.LoginResponse{Jwt: tokenString}, nil
